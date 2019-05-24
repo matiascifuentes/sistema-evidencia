00a18be20f437179d07f13f5582cb88938286295
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2019-05-24 11:55:00 -0400

    Correción migración evidencias.

--- database/migrations/2019_05_24_023649_create_evidencias_table.php
+++ database/migrations/2019_05_24_023649_create_evidencias_table.php
@@ -20,10 +20,12 @@ class CreateEvidenciasTable extends Migration
             $table->string('estado');
             $table->integer('nivel');
             $table->unsignedInteger('folio_id');
+            $table->string('codigo_car')->unsigned();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('formulario_id')->references('id')->on('formularios')->onDelete('cascade');
             $table->foreign('folio_id')->references('id')->on('folios')->onDelete('cascade');
+            $table->foreign('codigo_car')->references('codigo_car')->on('carreras')->onDelete('cascade');
 
             $table->timestamps();
         });
