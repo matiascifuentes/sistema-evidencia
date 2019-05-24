9694d23dafd68f8560471160a8c5589ed2e5a709
wonderwisee <pipeiciucm@gmail.com>
2019-05-23 22:44:14 -0400

    migraciones incluidas

---
+++ database/migrations/2019_05_24_023649_create_evidencias_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEvidenciasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evidencias', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('formulario_id');
+            $table->string('estado');
+            $table->integer('nivel');
+            $table->unsignedInteger('folio_id');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('formulario_id')->references('id')->on('formularios')->onDelete('cascade');
+            $table->foreign('folio_id')->references('id')->on('folios')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evidencias');
+    }
+}
