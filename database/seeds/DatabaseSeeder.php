2872fb49f98485e3a44e2348aacaa806e0ed797d
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2019-05-24 13:03:02 -0400

    Seeders para tablas alcance, ambito y tipo.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(RoleTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(AmbitoTableSeeder::class);
+        $this->call(AlcanceTableSeeder::class);
+        $this->call(TipoTableSeeder::class);
     }
 }
