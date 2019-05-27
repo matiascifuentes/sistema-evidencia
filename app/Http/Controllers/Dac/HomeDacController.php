882586e0a4ff111c27af20a02c737206f4359689
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2019-05-27 17:13:30 -0400

    Funcionalidad orden de prioridad agregada a Dac.

--- app/Http/Controllers/Dac/HomeDacController.php
+++ app/Http/Controllers/Dac/HomeDacController.php
@@ -23,7 +23,7 @@ class HomeDacController extends Controller
                                 ->join('profesor','evidencias.user_id','=','profesor.user_id')
                                 ->join('formularios','evidencias.formulario_id','=','formularios.id')
                                 ->join('carreras','evidencias.codigo_car','=','carreras.codigo_car')
-                                ->select('profesor.*','formularios.fecha_realizacion','formularios.titulo','carreras.nombre_car','formularios.id')
+                                ->select('profesor.*','formularios.fecha_realizacion','formularios.titulo','carreras.nombre_car','formularios.id','evidencias.created_at as fecha_creacion')
                                 ->paginate(8);
         return view('dac.home',["evidencias"=>$evidencias]);
     }
