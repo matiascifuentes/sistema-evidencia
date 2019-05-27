f7a878eea07a203c268b77544238a5d387f1fd1f
Matías Cifuentes Lara <matiascifuenteslara@gmail.com>
2019-05-27 17:05:01 -0400

    Funcionalidad orden de prioridad agregada a Revisor.

--- app/Http/Controllers/Revisor/HomeRevisorController.php
+++ app/Http/Controllers/Revisor/HomeRevisorController.php
@@ -23,7 +23,7 @@ class HomeRevisorController extends Controller
                                 ->join('profesor','evidencias.user_id','=','profesor.user_id')
                                 ->join('formularios','evidencias.formulario_id','=','formularios.id')
                                 ->join('carreras','evidencias.codigo_car','=','carreras.codigo_car')
-                                ->select('profesor.*','formularios.fecha_realizacion','formularios.titulo','carreras.nombre_car','formularios.id')
+                                ->select('profesor.*','formularios.fecha_realizacion','formularios.titulo','carreras.nombre_car','formularios.id','evidencias.created_at as fecha_creacion')
                                 ->paginate(8);
         return view('revisor.home',["evidencias"=>$evidencias]);
     }
--- resources/views/revisor/home.blade.php
+++ resources/views/revisor/home.blade.php
@@ -26,45 +26,195 @@
 <br>
 
 <div class="container">
-    <div class="col-md-6">
-       @if($evidencias->count())
-       @foreach($evidencias as $evidencia)
-       <div class="shadow-lg mb-4 bg-white card">
-            <div class="card-header bg-info">
-                <div class="row">
-                    <div class="col-md-4 bg-white text-center">{{$evidencia->run}}</div>
-                    <div class="col-md-4"></div>
-                    <div class="col-md-4 bg-white text-center">{{$evidencia->fecha_realizacion}}</div>                  
-                </div>
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-4">Profesor: </div>
-                    <div class="col-md-8">
-                        {{$evidencia->nombre1}} {{$evidencia->nombre2}} {{$evidencia->apellido1}} {{$evidencia->apellido2}}
-                    </div>
-                </div> 
-                <div class="row">
-                    <div class="col-md-4">Carrera: </div>
-                    <div class="col-md-8">{{$evidencia->nombre_car}}</div>
-                </div> 
-                <div class="row">
-                    <div class="col-md-4">Título: </div>
-                    <div class="col-md-8">{{$evidencia->titulo}}</div>
-                </div>
-            </div>
-            <div class="card-footer text-center">
-                <a class="btn btn-success btn-block" href="{{route('formularioEvidencia-show',$evidencia->id)}}">Revisar formulario</a>
-            </div>
-        </div>
-        @endforeach
-        {!!$evidencias->render()!!}
-        @else
-            <div class="alert alert-info">
-                <strong>¡No hay evidencias!</strong> En este momento no hay evidencias pendientes de revisión.
-            </div>
-        @endif
 
-    </div> 
+    <h2>Prioridad por</h2>
+    <!-- Nav pills -->
+    <ul class="nav nav-pills" role="tablist">
+        <li class="nav-item">
+            <a class="nav-link active" data-toggle="pill" href="#orden1">Fecha de realización ASC</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="pill" href="#orden2">Fecha de realización DESC</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="pill" href="#orden3">Fecha de creación ASC</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="pill" href="#orden4">Fecha de creación DESC</a>
+        </li>
+    </ul>
+
+  <!-- Tab panes -->
+    <div class="tab-content">
+        <div id="orden1" class="container tab-pane active"><br>
+
+            <div class="col-md-6">
+               @if($evidencias->count())
+               @foreach($evidencias->sortBy('fecha_realizacion') as $evidencia)
+               <div class="shadow-lg mb-4 bg-white card">
+                    <div class="card-header bg-info">
+                        <div class="row">
+                            <div class="col-md-4 bg-white text-center">{{$evidencia->run}}</div>
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4 bg-white text-center">{{$evidencia->fecha_realizacion}}</div>                  
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-4">Profesor: </div>
+                            <div class="col-md-8">
+                                {{$evidencia->nombre1}} {{$evidencia->nombre2}} {{$evidencia->apellido1}} {{$evidencia->apellido2}}
+                            </div>
+                        </div> 
+                        <div class="row">
+                            <div class="col-md-4">Carrera: </div>
+                            <div class="col-md-8">{{$evidencia->nombre_car}}</div>
+                        </div> 
+                        <div class="row">
+                            <div class="col-md-4">Título: </div>
+                            <div class="col-md-8">{{$evidencia->titulo}}</div>
+                        </div>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a class="btn btn-success btn-block" href="{{route('formularioEvidencia-show',$evidencia->id)}}">Revisar formulario</a>
+                    </div>
+                </div>
+                @endforeach
+                {!!$evidencias->render()!!}
+                @else
+                    <div class="alert alert-info">
+                        <strong>¡No hay evidencias!</strong> En este momento no hay evidencias pendientes de revisión.
+                    </div>
+                @endif
+            </div> 
+        </div>
+        <div id="orden2" class="container tab-pane fade"><br>
+
+            <div class="col-md-6">
+               @if($evidencias->count())
+               @foreach($evidencias->sortByDesc('fecha_realizacion') as $evidencia)
+               <div class="shadow-lg mb-4 bg-white card">
+                    <div class="card-header bg-info">
+                        <div class="row">
+                            <div class="col-md-4 bg-white text-center">{{$evidencia->run}}</div>
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4 bg-white text-center">{{$evidencia->fecha_realizacion}}</div>                  
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-4">Profesor: </div>
+                            <div class="col-md-8">
+                                {{$evidencia->nombre1}} {{$evidencia->nombre2}} {{$evidencia->apellido1}} {{$evidencia->apellido2}}
+                            </div>
+                        </div> 
+                        <div class="row">
+                            <div class="col-md-4">Carrera: </div>
+                            <div class="col-md-8">{{$evidencia->nombre_car}}</div>
+                        </div> 
+                        <div class="row">
+                            <div class="col-md-4">Título: </div>
+                            <div class="col-md-8">{{$evidencia->titulo}}</div>
+                        </div>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a class="btn btn-success btn-block" href="{{route('formularioEvidencia-show',$evidencia->id)}}">Revisar formulario</a>
+                    </div>
+                </div>
+                @endforeach
+                {!!$evidencias->render()!!}
+                @else
+                    <div class="alert alert-info">
+                        <strong>¡No hay evidencias!</strong> En este momento no hay evidencias pendientes de revisión.
+                    </div>
+                @endif
+            </div>           
+        </div>
+        <div id="orden3" class="container tab-pane active"><br>
+
+            <div class="col-md-6">
+               @if($evidencias->count())
+               @foreach($evidencias->sortBy('fecha_creacion') as $evidencia)
+               <div class="shadow-lg mb-4 bg-white card">
+                    <div class="card-header bg-info">
+                        <div class="row">
+                            <div class="col-md-4 bg-white text-center">{{$evidencia->run}}</div>
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4 bg-white text-center">{{$evidencia->fecha_realizacion}}</div>                  
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-4">Profesor: </div>
+                            <div class="col-md-8">
+                                {{$evidencia->nombre1}} {{$evidencia->nombre2}} {{$evidencia->apellido1}} {{$evidencia->apellido2}}
+                            </div>
+                        </div> 
+                        <div class="row">
+                            <div class="col-md-4">Carrera: </div>
+                            <div class="col-md-8">{{$evidencia->nombre_car}}</div>
+                        </div> 
+                        <div class="row">
+                            <div class="col-md-4">Título: </div>
+                            <div class="col-md-8">{{$evidencia->titulo}}</div>
+                        </div>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a class="btn btn-success btn-block" href="{{route('formularioEvidencia-show',$evidencia->id)}}">Revisar formulario</a>
+                    </div>
+                </div>
+                @endforeach
+                {!!$evidencias->render()!!}
+                @else
+                    <div class="alert alert-info">
+                        <strong>¡No hay evidencias!</strong> En este momento no hay evidencias pendientes de revisión.
+                    </div>
+                @endif
+            </div> 
+        </div>
+        <div id="orden4" class="container tab-pane fade"><br>
+
+            <div class="col-md-6">
+               @if($evidencias->count())
+               @foreach($evidencias->sortByDesc('fecha_creacion') as $evidencia)
+               <div class="shadow-lg mb-4 bg-white card">
+                    <div class="card-header bg-info">
+                        <div class="row">
+                            <div class="col-md-4 bg-white text-center">{{$evidencia->run}}</div>
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4 bg-white text-center">{{$evidencia->fecha_realizacion}}</div>                  
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-4">Profesor: </div>
+                            <div class="col-md-8">
+                                {{$evidencia->nombre1}} {{$evidencia->nombre2}} {{$evidencia->apellido1}} {{$evidencia->apellido2}}
+                            </div>
+                        </div> 
+                        <div class="row">
+                            <div class="col-md-4">Carrera: </div>
+                            <div class="col-md-8">{{$evidencia->nombre_car}}</div>
+                        </div> 
+                        <div class="row">
+                            <div class="col-md-4">Título: </div>
+                            <div class="col-md-8">{{$evidencia->titulo}}</div>
+                        </div>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a class="btn btn-success btn-block" href="{{route('formularioEvidencia-show',$evidencia->id)}}">Revisar formulario</a>
+                    </div>
+                </div>
+                @endforeach
+                {!!$evidencias->render()!!}
+                @else
+                    <div class="alert alert-info">
+                        <strong>¡No hay evidencias!</strong> En este momento no hay evidencias pendientes de revisión.
+                    </div>
+                @endif
+            </div>           
+        </div>
+    </div>
 </div>
+
 @endsection
