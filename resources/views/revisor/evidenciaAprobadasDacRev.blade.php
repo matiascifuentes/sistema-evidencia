5e8e572d2f1afbeb3b78cd101f21df8a72d0173d
Javier <ngn.vidal@gmail.com>
2019-05-30 12:12:19 -0400

    Ahora si , estaban sin seguimientos algunos archivos

---
+++ resources/views/revisor/evidenciaAprobadasDacRev.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-8 containerblue text-center">
+				<span>EVIDENCIAS APROBADAS </span>
+			</div>
+		</div>
+    <div class="row justify-content-center">
+    	<div class="col-md-8 paddingesp ">
+			<div id="pricing-table" class="clear row">
+					 @foreach($evidencias as $evidencia)
+					<div class="plan">
+				        <h3>EVID-{{$evidencia->id}}-{{$evidencia->codigo_car}}<span><i class="fa fa-check fa-2x fa-fw"></i></span></h3>
+				        <a class="signup" href="{{route('evidenciaAprobada',$evidencia->id)}}">Ver Evidencia</a>         
+				        <ul>	
+				        	<li><b>Titulo</b><br>{{$evidencia->titulo}}</li>	
+				            <li><b>Run</b><br>{{$evidencia->run}}</li>
+				            <li><b>Fecha</b><br> {{$evidencia->fecha_realizacion}}</li>
+				            <li><b>Profesor</b><br>{{$evidencia->nombre1}} {{$evidencia->nombre2}} {{$evidencia->apellido1}} {{$evidencia->apellido2}}</li>
+				            <li><b>Carrera</b><br>{{$evidencia->nombre_car}}</li>					
+				        </ul> 
+				    </div>
+					 @endforeach
+				</div>
+		</div>
+	</div>
+</div>
+@endsection
---
+++ resources/views/revisor/evidenciaNoAprobadas.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="card">
+    <div class="card-header" 
+         style="text-align:center;font-size:18px;font-wight:bold;">
+            EVIDENCIAS NO APROBADAS</div>
+
+</div>
+<br>
+<div class="container">
+    <div class="col-md-6" style="margin-left:280px">
+       @if($evidencias->count())
+       @foreach($evidencias as $evidencia)
+       <div class="shadow-lg mb-4 bg-white card">
+            <div class="card-header bg-info">
+                <div class="row">
+                    <div class="col-md-4 bg-white text-center">{{$evidencia->run}}</div>
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4 bg-white text-center">{{$evidencia->fecha_realizacion}}</div>                  
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-4">Profesor: </div>
+                    <div class="col-md-8">
+                        {{$evidencia->nombre1}} {{$evidencia->nombre2}} {{$evidencia->apellido1}} {{$evidencia->apellido2}}
+                    </div>
+                </div> 
+                <div class="row">
+                    <div class="col-md-4">Carrera: </div>
+                    <div class="col-md-8">{{$evidencia->nombre_car}}</div>
+                </div> 
+                <div class="row">
+                    <div class="col-md-4">Título: </div>
+                    <div class="col-md-8">{{$evidencia->titulo}}</div>
+                </div>
+            </div>
+            <div class="card-footer text-center">
+                <a class="btn btn-success btn-block" href="{{route('evidenciaAprobada',$evidencia->id)}}">Revisar formulario</a>
+            </div>
+        </div>
+        @endforeach
+        @else
+            <div class="alert alert-info">
+                <strong>¡No hay evidencias!</strong> En este momento no hay evidencias rechazadas.
+            </div>
+        @endif
+    </div> 
+</div>
+@endsection
